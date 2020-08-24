fe8e73059fe7d55af998dc1926d5460b7a4bc813
EmanMohamedHassen <emy_eva23@yahoo.com>
2020-08-24 05:29:10 +0200

    test for successful operation and for expected errors

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}:{}@{}/{}".format('eman','em1234','localhost:5434',self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,7 +33,30 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_questions(self):
+        result = self.client().get('/questions')
+        data=json.loads(result.data)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['categories'])
 
+    def test_get_categories(self):
+        result = self.client().get('/categories')
+        data=json.loads(result.data)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+        self.assertTrue(data['total_categories'])
+
+    def test_404_delete_question(self):
+        result = self.client().delete('/questions/100')
+        data=json.loads(result.data)
+        question = Question.query.filter(Question.id == 100).one_or_none()
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['deleted_question'],100)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
