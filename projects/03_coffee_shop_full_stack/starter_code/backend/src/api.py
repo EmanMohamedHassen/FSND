db3d7f05dbafcf3a9a5bd8cf27f82383f3621da9
EmanMohamedHassen <emy_eva23@yahoo.com>
2020-09-09 03:04:19 +0200

    code for PEP8 requirements

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -18,21 +18,23 @@ THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
 # db_drop_and_create_all()
 
-## ROUTES
+# ROUTES
 '''
     GET /drinks
         a public endpoint
         returns a list of drinks
 '''
-@app.route('/drinks',methods=['GET'])
+
+
+@app.route('/drinks', methods=['GET'])
 def get_drinks():
     drinks = Drink.query.all()
     formatted_drinks = [drink.short() for drink in drinks]
-    if len(formatted_drinks) == 0 :
+    if len(formatted_drinks) == 0:
         abort(404)
 
     return jsonify({
-        'success':True,
+        'success': True,
         "drinks": formatted_drinks
     })
 
@@ -41,16 +43,18 @@ def get_drinks():
         require the 'get:drinks-detail' permission
         returns a list of drinks details
 '''
-@app.route('/drinks-detail',methods=['GET'])
+
+
+@app.route('/drinks-detail', methods=['GET'])
 @requires_auth('get:drinks-detail')
 def get_drinks_details():
     drinks = Drink.query.all()
     formatted_drinks = [drink.long() for drink in drinks]
-    if len(formatted_drinks) == 0 :
+    if len(formatted_drinks) == 0:
         abort(404)
 
     return jsonify({
-        'success':True,
+        'success': True,
         "drinks": formatted_drinks
     })
 
@@ -60,18 +64,20 @@ def get_drinks_details():
         require the 'post:drinks' permission
         returns a list of drinks details
 '''
-@app.route('/drinks',methods=['POST'])
+
+
+@app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
 def add_drink():
     body = request.get_json()
-    title= body.get('title',None)
-    recipe = body.get('recipe',None)
+    title = body.get('title', None)
+    recipe = body.get('recipe', None)
     try:
         drink = Drink(title=title, recipe=json.dumps(recipe))
         drink.insert()
 
         return jsonify({
-            'success':True,
+            'success': True,
             "drinks": drink.long()
         })
     except:
@@ -83,25 +89,26 @@ def add_drink():
         require the 'patch:drinks' permission
         returns a list of drinks details
 '''
-@app.route('/drinks/<id>',methods=['PATCH'])
+
+
+@app.route('/drinks/<id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
-def edit_drink(id) :
+def edit_drink(id):
     body = request.get_json()
-    title= body.get('title',None)
-    recipe = body.get('recipe',None)
+    title = body.get('title', None)
+    recipe = body.get('recipe', None)
     try:
         drink = Drink.query.filter(Drink.id == id).one_or_none()
-        if drink is None :
+        if drink is None:
             abort(404)
         if title is not None:
             drink.title = title
         if recipe is not None:
-            drink.recipe=json.dumps(recipe)        
+            drink.recipe = json.dumps(recipe)
         drink.update()
-        
         return jsonify({
-        'success': True,
-        "drinks": drink.long()
+             'success': True,
+             "drinks": drink.long()
         })
     except:
         abort(422)
@@ -113,61 +120,68 @@ def edit_drink(id) :
         returns  the id of the deleted record
 '''
 
-@app.route('/drinks/<id>',methods=['DELETE'])
+
+@app.route('/drinks/<id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
-def delete_drink(id) :
+def delete_drink(id):
     try:
         drink = Drink.query.filter(Drink.id == id).one_or_none()
 
-        if drink is None :
+        if drink is None:
             abort(404)
         drink.delete()
-        
         return jsonify({
-        'success': True,
-        "delete": id
+                'success': True,
+                "delete": id
         })
     except:
         abort(422)
-## Error Handling
+# Error Handling
 '''
 Example error handling for unprocessable entity
 '''
+
+
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 422,
                     "message": "unprocessable"
                     }), 422
 
+
 @app.errorhandler(400)
 def unprocessable(error):
     return jsonify({
-        "success": False, 
+        "success": False,
         "error": 400,
         "message": "bad request"
         }), 400
 
+
 @app.errorhandler(405)
 def unprocessable(error):
     return jsonify({
-        "success": False, 
+        "success": False,
         "error": 405,
         "message": "method not allowed"
-        }), 405   
+        }), 405
+
+
 @app.errorhandler(500)
 def unprocessable(error):
     return jsonify({
-        "success": False, 
+        "success": False,
         "error": 500,
         "message": "Internal Server Error"
-        }), 500       
+        }), 500
+
 
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({
-        "success": False, 
+        "success": False,
         "error": 404,
         "message": "resource not found"
         }), 404
@@ -175,10 +189,12 @@ def not_found(error):
 '''
 error handler for AuthError
 '''
+
+
 @app.errorhandler(AuthError)
 def unAuthorized(error):
     return jsonify({
-        "success": False, 
+        "success": False,
         "error": error.status_code,
         "message": error.error
         }), error.status_code
